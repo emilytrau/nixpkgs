8e221a6737b45f36ec808ca9301d336f25f7a3d4
Tristan Ross <tristan.ross@midstall.com>
2024-05-14 21:36:09 -0700

    buildFlutterApplication: add macos support

--- pkgs/build-support/flutter/default.nix
+++ pkgs/build-support/flutter/default.nix
@@ -11,13 +11,15 @@
 , jq
 , yq
 , moreutils
+, targetPlatform
 }:
-
-# absolutely no mac support for now
 
 { pubGetScript ? "flutter pub get"
 , flutterBuildFlags ? [ ]
-, targetFlutterPlatform ? "linux"
+, targetFlutterPlatform ?
+  if targetPlatform.isLinux then "linux"
+  else if targetPlatform.isDarwin then "macos"
+  else "universal"
 , extraWrapProgramArgs ? ""
 , ...
 }@args:
@@ -48,7 +50,7 @@ let
 
         export HOME="$NIX_BUILD_TOP"
         flutter config --no-analytics &>/dev/null # mute first-run
-        flutter config --enable-linux-desktop >/dev/null
+        flutter config --enable-${targetFlutterPlatform}-desktop >/dev/null
       '';
 
       inherit pubGetScript;
@@ -146,6 +148,44 @@ let
       extraWrapProgramArgs = ''
         ''${gappsWrapperArgs[@]} \
         ${extraWrapProgramArgs}
+      '';
+    };
+
+    macos = universal // {
+      outputs = universal.outputs or [ ] ++ [ "debug" ];
+
+      nativeBuildInputs = (universal.nativeBuildInputs or [ ]);
+      buildInputs = (universal.buildInputs or [ ]);
+
+      dontDartBuild = true;
+      buildPhase = universal.buildPhase or ''
+        runHook preBuild
+
+        mkdir -p build/flutter_assets/fonts
+
+        flutter build macos -v --release --split-debug-info="$debug" ${builtins.concatStringsSep " " (map (flag: "\"${flag}\"") flutterBuildFlags)}
+
+        runHook postBuild
+      '';
+
+      dontDartInstall = true;
+      installPhase = universal.installPhase or ''
+        runHook preInstall
+
+        built=build/macos/*/release/bundle
+
+        mkdir -p $out/bin
+        mv $built $out/app
+
+        for f in $(find $out/app -iname "*.desktop" -type f); do
+          install -D $f $out/share/applications/$(basename $f)
+        done
+
+        for f in $(find $out/app -maxdepth 1 -type f); do
+          ln -s $f $out/bin/$(basename $f)
+        done
+
+        runHook postInstall
       '';
     };
 
