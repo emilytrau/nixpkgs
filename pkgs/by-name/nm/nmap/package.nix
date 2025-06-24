11106026c5e1fc3d6a409339630e5cfe88a07a4f
Emily Trau <emily@downunderctf.com>
2025-06-24 14:56:59 -0400

    nmap: re-enable zenmap and ndiff

--- pkgs/by-name/nm/nmap/package.nix
+++ pkgs/by-name/nm/nmap/package.nix
@@ -3,6 +3,7 @@
   stdenv,
   fetchurl,
   versionCheckHook,
+  makeBinaryWrapper,
   libpcap,
   pkg-config,
   openssl,
@@ -11,38 +12,72 @@
   liblinear,
   libssh2,
   zlib,
+  python3,
+  python3Packages,
+  wrapGAppsHook3,
+  writeDarwinBundle,
   withLua ? true,
+  graphicalSupport ? true,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "nmap";
   version = "7.97";
 
   src = fetchurl {
-    url = "https://nmap.org/dist/nmap-${version}.tar.bz2";
+    url = "https://nmap.org/dist/nmap-${finalAttrs.version}.tar.bz2";
     hash = "sha256-r5jyeSXGcMJX3Zap3fJyTgbLebL9Hg0IySBjFr4WRcA=";
   };
 
-  prePatch = lib.optionalString stdenv.hostPlatform.isDarwin ''
+  postPatch = ''
     substituteInPlace libz/configure \
-        --replace /usr/bin/libtool ar \
-        --replace 'AR="libtool"' 'AR="ar"' \
-        --replace 'ARFLAGS="-o"' 'ARFLAGS="-r"'
+        --replace-fail /usr/bin/libtool ar \
+        --replace-fail 'AR="libtool"' 'AR="ar"' \
+        --replace-fail 'ARFLAGS="-o"' 'ARFLAGS="-r"'
+    substituteInPlace zenmap/zenmapCore/Paths.py \
+        --replace-fail "/usr/local/bin" "$out/bin"
   '';
+
+  patches = [
+    # Fix python install prefix
+    ./zenmap-python-env.patch
+  ];
 
   configureFlags = [
     (if withLua then "--with-liblua=${lua5_4}" else "--without-liblua")
-    "--without-ndiff"
-    "--without-zenmap"
+    (lib.withFeature graphicalSupport "zenmap")
   ];
 
   postInstall = ''
     install -m 444 -D nselib/data/passwords.lst $out/share/wordlists/nmap.lst
   '';
 
-  postFixup = lib.optionalString stdenv.hostPlatform.isDarwin ''
-    install_name_tool -change liblinear.so.5 ${liblinear.out}/lib/liblinear.5.dylib $out/bin/nmap
-  '';
+  preFixup =
+    ''
+      buildPythonPath "$out $propagatedBuildInputs"
+      wrapProgram $out/bin/ndiff \
+        --prefix PYTHONPATH : "$program_PYTHONPATH"
+    ''
+    + lib.optionalString stdenv.hostPlatform.isDarwin ''
+      install_name_tool -change liblinear.so.5 ${liblinear.out}/lib/liblinear.5.dylib $out/bin/nmap
+    ''
+    + lib.optionalString graphicalSupport ''
+      wrapProgram $out/bin/zenmap \
+        --prefix PYTHONPATH : "$program_PYTHONPATH" \
+        --prefix GTK_PATH : "$out/lib/gtk-2.0" \
+        "''${gappsWrapperArgs[@]}"
+      install -m 444 -D zenmap/zenmapCore/data/pixmaps/zenmap.png $out/share/pixmaps/zenmap.png
+    ''
+    + lib.optionalString (graphicalSupport && stdenv.hostPlatform.isDarwin) ''
+      mkdir -p "$out/Applications/Zenmap.app/Contents/MacOS"
+      mkdir -p "$out/Applications/Zenmap.app/Contents/Resources"
+      cp zenmap/install_scripts/macosx/zenmap.icns "$out/Applications/Zenmap.app/Contents/Resources/zenmap.icns"
+      write-darwin-bundle "$out" "Zenmap" "zenmap" "zenmap" "false"
+      mv "$out/Applications/Zenmap.app/Contents/MacOS/Zenmap" "$out/Applications/Zenmap.app/Contents/MacOS/zenmap.bin"
+      ${stdenv.cc.targetPrefix}cc "zenmap/install_scripts/macosx/zenmap_auth.m" -lobjc -framework Foundation -o "$out/Applications/Zenmap.app/Contents/MacOS/Zenmap"
+    '';
+
+  dontWrapGApps = true;
 
   makeFlags = lib.optionals (stdenv.buildPlatform != stdenv.hostPlatform) [
     "AR=${stdenv.cc.bintools.targetPrefix}ar"
@@ -50,7 +85,22 @@ stdenv.mkDerivation rec {
     "CC=${stdenv.cc.targetPrefix}gcc"
   ];
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs =
+    [
+      pkg-config
+      python3
+      python3Packages.wrapPython
+      python3Packages.build
+      python3Packages.pip
+      python3Packages.setuptools
+      makeBinaryWrapper
+    ]
+    ++ lib.optionals graphicalSupport [
+      wrapGAppsHook3
+    ]
+    ++ lib.optionals (graphicalSupport && stdenv.hostPlatform.isDarwin) [
+      writeDarwinBundle
+    ];
   buildInputs = [
     pcre2
     liblinear
@@ -58,6 +108,9 @@ stdenv.mkDerivation rec {
     libpcap
     openssl
     zlib
+  ];
+  propagatedBuildInputs = lib.optionals graphicalSupport [
+    python3Packages.pygobject3
   ];
 
   enableParallelBuilding = true;
@@ -73,12 +126,13 @@ stdenv.mkDerivation rec {
   meta = {
     description = "Free and open source utility for network discovery and security auditing";
     homepage = "http://www.nmap.org";
-    changelog = "https://nmap.org/changelog.html#${version}";
+    changelog = "https://nmap.org/changelog.html#${finalAttrs.version}";
     license = lib.licenses.gpl2Only;
     platforms = lib.platforms.all;
+    mainProgrem = "nmap";
     maintainers = with lib.maintainers; [
       thoughtpolice
       fpletz
     ];
   };
-}
+})
